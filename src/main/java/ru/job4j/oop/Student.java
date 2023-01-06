c04e6540a499d93b36fe56398924442e6c46f2dc
evgen <evgenii_melchugov@mail.ru>
2023-01-06 12:33:39 +0700

    Исправление замечаний

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -3,7 +3,10 @@ package ru.job4j.oop;
 public class Student {
     public void song() {
         System.out.println("I believe I can fly");
+    }
 
+    public void music() {
+        System.out.println("Tra tra tra");
     }
 
     public static void main(String[] args) {
@@ -11,5 +14,8 @@ public class Student {
         petya.song();
         petya.song();
         petya.song();
+        petya.music();
+        petya.music();
+        petya.music();
     }
 }
